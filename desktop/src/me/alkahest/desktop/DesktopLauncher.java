33aa0c26b720db52624d9c2fe62fb426d86b3eb3
Muhammad Khan <makowns@hotmail.com>
2017-07-02 17:24:03 -0400

    GDX Setup
    package: me.alkahest

---
+++ desktop/src/me/alkahest/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package me.alkahest.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import me.alkahest.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
